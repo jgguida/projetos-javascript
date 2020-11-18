4dcf09556f4ac00ddbd2144401149f688f6245d2
Jon Guida <jonguida@localhost.localdomain>
2020-11-18 18:37:22 -0300

    Let's call it a day

---
+++ urna-eletronica/etapas.js
@@ -0,0 +1,46 @@
+let etapas = [
+    {
+        titulo : 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+             
+                numero:38111,
+                nome: 'Fulano',
+                partido: 'ABCD',
+                fotos:[
+                    {url:'img/cp1.jpg', legenda: 'VEREADOR' }
+                ]
+                
+            },
+            {
+             
+                numero:38112,
+                nome: 'Fulano',
+                partido: 'ABCD',
+                fotos:[
+                    {url:'img/cp2.jpg', legenda: 'VEREADOR' }
+                ]
+                
+            }
+        ]
+    },
+    {
+    titulo:'PREFEITO',
+    numeros: 2,
+    candidatos: [
+        {
+            numero:99,
+            nome:'Ciclano',
+            partido:'Abcd',
+            vice:'CIC',
+            fotos:[
+                { url: 'img/cp4.jpg', legenda: 'PREFEITO'},
+                { url: 'img/cp5.jpg', legenda: 'PREFEITO'}
+
+            ]
+        }
+    ]
+    }
+       
+]
